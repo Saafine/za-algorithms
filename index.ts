cbd46b7d8409b30777a4c57826841644f15107fc
Labus, Pawel <pawel.labus@falkbuilt.com>
2021-06-12 11:00:02 +0200

    Up

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import { run3cycleColoring } from './3-cycle-coloring/main';
 
 // 3-cycle-coloring
-run3cycleColoring([17, 61, 44, 38, 54, 27, 13, 22]);
+const input = [17, 61, 44, 38, 54, 27, 13, 22];
+console.log("Running for input", input);
+run3cycleColoring(input);
