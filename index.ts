66953d2374cd5897b8c5c5f96c7ff1193b773f27
Saafine <pawel.lbs@gmail.com>
2021-06-12 12:22:33 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ console.clear();
 
 // 3-cycle-coloring
 if (true) {
-  const input = [17, 61, 44, 38, 54, 27, 13, 22];
+  // const input = [17, 61, 44, 38, 54, 27, 13, 22];
+  // const input = [77, 36, 12, 62, 25, 18, 9, 61];
+  const input = [50, 3, 19, 15, 53, 33, 37, 14];
   console.log("Running for input", input);
   run3cycleColoring(input);
 }
